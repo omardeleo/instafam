830abdaef42ae8123e4b3f41e7795b6e6a1ac3a4
Omar De Leo <omar.deleo@gmail.com>
2019-04-09 11:28:43 -0400

    Create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: {format: :json} do
+    resources :users, only: [:create, :show, :index, :update]
+    resource :session, only: [:create, :destroy]
+    # resources :posts, only: [:create, :destroy, :show, :index, :update] do
+    #   resources :likes, only: [:create, :destroy]
+    #   resources :comments, only: [:create, :index]
+    # end
+    # resources :follows, only: [:create]
+    # resources :comments, only: [:destroy]
+  end
+  
   root 'static_pages#root'
 end
