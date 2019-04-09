77b602d41c7bbd83de05e6cd2516ecd6790ee86a
Omar De Leo <omar.deleo@gmail.com>
2019-04-09 17:22:11 -0400

    Configure preloadedState

--- frontend/instafam.jsx
+++ frontend/instafam.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import configureStore from './store/store';
+import Root from './components/root';
+import { login, logout } from './actions/session_actions';
+
 document.addEventListener('DOMContentLoaded', () => {
+    let store;
+    if (window.currentUser) {
+        const preloadedState = {
+            entities: {
+                users: { [window.currentUser.id]: window.currentUser }
+            },
+            session: { id: window.currentUser.id }
+        };
+        store = configureStore(preloadedState);
+        delete window.currentUser;
+    } else {
+        store = configureStore();
+    }
+
+    //TEST BEGIN
+    window.login = login;
+    window.logout = logout;
+    window.dispatch = store.dispatch;
+    window.getState = store.getState;
+    //TEST END
+    
     const root = document.getElementById('root');
-    ReactDOM.render(<h1>Welcome to Instafam</h1>, root);
-})
+    ReactDOM.render(<Root store={store} />, root);
+});
