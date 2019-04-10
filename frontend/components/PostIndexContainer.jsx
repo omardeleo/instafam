06945af343683cf266ba29790128de3817936966
Omar De Leo <omar.deleo@gmail.com>
2019-04-10 10:28:19 -0400

    Create PostIndexContainer component

---
+++ frontend/components/PostIndexContainer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const PostIndexContainer = () => {
+    return (
+        <div>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <h1>TEST</h1>
+        </div>
+    )
+}
+
+export default PostIndexContainer;
