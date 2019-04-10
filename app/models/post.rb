56a4330e9b831a7d46aa7495796f581c65410990
Omar De Leo <omar.deleo@gmail.com>
2019-04-10 11:18:49 -0400

    Create post model

---
+++ app/models/post.rb
@@ -0,0 +1,15 @@
+class Post < ApplicationRecord
+  validates :author_id, presence: true
+
+  belongs_to :author,
+    foreign_key: :author_id,
+    class_name: :User
+
+#   has_many :likes
+
+#   has_many :likers,
+#     through: :likes,
+#     source: :liker
+
+#   has_many :comments
+end
