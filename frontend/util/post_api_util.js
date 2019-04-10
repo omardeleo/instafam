428f2c53aaac8941e94e987127676effdc184d9b
Omar De Leo <omar.deleo@gmail.com>
2019-04-10 13:55:19 -0400

    Create post API util

---
+++ frontend/util/post_api_util.js
@@ -0,0 +1,46 @@
+export const fetchPosts = () => {
+    return $.ajax({
+        method: 'GET',
+        url: '/api/posts',
+    });
+};
+
+export const fetchUserPosts = (id) => {
+    return $.ajax({
+        method: 'GET',
+        url: '/api/posts',
+        data: { id }
+    });
+};
+
+export const fetchPost = (id) => {
+    return $.ajax({
+        method: 'GET',
+        url: `/api/posts/${id}`,
+    });
+};
+
+// export const createPost = (formData) => {
+//     return $.ajax({
+//         method: 'POST',
+//         url: 'api/posts',
+//         data: formData,
+//         processData: false,
+//         contentType: false,
+//     });
+// };
+
+// export const deletePost = (id) => {
+//     return $.ajax({
+//         method: 'DELETE',
+//         url: `/api/posts/${id}`,
+//     });
+// };
+
+// export const createComment = comment => {
+//     return $.ajax({
+//         method: 'POST',
+//         url: 'api/comments',
+//         data: { comment }
+//     });
+// };
