e49d8995f7fdee9d66a49ad9bd4fbc4ade11ebf1
Omar De Leo <omar.deleo@gmail.com>
2019-04-10 10:57:12 -0400

    Create posts table

---
+++ db/migrate/20190410145224_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.integer :author_id, null: false
+      t.text :caption
+      t.string :location
+      
+      t.timestamps
+    end
+
+    add_index :posts, :author_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_09_150106) do
+ActiveRecord::Schema.define(version: 2019_04_10_145224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "author_id", null: false
+    t.text "caption"
+    t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", limit: 30, null: false
