0c13e1580f2dcaade15a003465852a2a8edc5c29
Omar De Leo <omar.deleo@gmail.com>
2019-04-10 15:06:49 -0400

    Update PostIndexContainer

--- frontend/components/PostIndexContainer.jsx
+++ frontend/components/PostIndexContainer.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 
-const PostIndexContainer = () => {
-    return (
-        <div>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <h1>TEST</h1>
-        </div>
-    )
-}
+import { connect } from 'react-redux';
+import { requestPosts, clearPosts } from '../actions/post_actions';
+import PostIndex from './PostIndex.jsx';
+// import { requestUsers, requestUser } from '../../actions/user_actions';
+// import { createLike, deleteLike } from '../../actions/like_actions';
 
-export default PostIndexContainer;
+const mapStateToProps = (state) => ({
+    posts: Object.values(state.entities.posts),
+    currentUserId: state.session.id,
+    users: state.entities.users,
+    comments: state.entities.comments,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    requestPosts: () => dispatch(requestPosts()),
+    createLike: (postId) => dispatch(createLike(postId)),
+    deleteLike: (postId, likeId) => dispatch(deleteLike(postId, likeId)),
+    clearPosts: () => dispatch(clearPosts())
+});
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(PostIndex);
