3a1165263d2124adc0bc26df7c378cd11dceeda5
Omar De Leo <omar.deleo@gmail.com>
2019-04-10 22:01:31 -0400

    Add comment actions

--- frontend/reducers/posts_reducer.js
+++ frontend/reducers/posts_reducer.js
@@ -3,12 +3,12 @@ import {
     RECEIVE_POST,
     CLEAR_POSTS
 } from '../actions/post_actions';
-// import { RECEIVE_COMMENT, DELETE_COMMENT } from '../actions/comment_actions';
+import { RECEIVE_COMMENT, DELETE_COMMENT } from '../actions/comment_actions';
 
 const postsReducer = (state = {}, action) => {
     Object.freeze(state);
     let newState;
-    // let post;
+    let post;
     switch (action.type) {
         case RECEIVE_POSTS:
             const posts = {};
@@ -21,16 +21,16 @@ const postsReducer = (state = {}, action) => {
             return Object.assign({}, state, newState);
         case CLEAR_POSTS:
             return {};
-        // case RECEIVE_COMMENT:
-        //     post = Object.assign({}, state[action.comment.post_id]);
-        //     post.commentIds = post.commentIds.concat(action.comment.id);
-        //     newState = Object.assign({}, state, { [action.comment.post_id]: post });
-        //     return newState;
-        // case DELETE_COMMENT:
-        //     post = Object.assign({}, state[action.comment.comment.post_id]);
-        //     post.commentIds = post.commentIds.filter(id => id !== action.comment.comment.id);
-        //     newState = Object.assign({}, state, { [action.comment.comment.post_id]: post });
-        //     return newState;
+        case RECEIVE_COMMENT:
+            post = Object.assign({}, state[action.comment.post_id]);
+            post.commentIds = post.commentIds.concat(action.comment.id);
+            newState = Object.assign({}, state, { [action.comment.post_id]: post });
+            return newState;
+        case DELETE_COMMENT:
+            post = Object.assign({}, state[action.comment.comment.post_id]);
+            post.commentIds = post.commentIds.filter(id => id !== action.comment.comment.id);
+            newState = Object.assign({}, state, { [action.comment.comment.post_id]: post });
+            return newState;
         default:
             return state;
     }
