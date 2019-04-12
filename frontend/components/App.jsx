e5f97d461d499c0078f5e24364d89426ebd2286b
Omar De Leo <omar.deleo@gmail.com>
2019-04-11 21:58:56 -0400

    Add routes to ProfileContainer

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -4,7 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import NavbarContainer from './NavbarContainer';
 import SessionFormContainer from './SessionFormContainer';
-// import ProfileContainer from './profile/profile_container';
+import ProfileContainer from './ProfileContainer';
 import PostIndexContainer from './PostIndexContainer';
 // import CreatePostFormContainer from './post/create_post_form_container';
 // import PostShowContainer from './post/post_show_container';
@@ -16,6 +16,8 @@ const App = () => (
         <Switch>
             <AuthRoute exact path="/login" component={SessionFormContainer} />
             <AuthRoute exact path="/signup" component={SessionFormContainer} />
+            <ProtectedRoute path="/profile" component={ProfileContainer} />
+            <Route path="/users/:userId" component={ProfileContainer} />
             <ProtectedRoute path="/" component={PostIndexContainer} />
         </Switch>
     </div>
