39316ffb82c74cbb5179098ebe7a32d218231a59
Omar De Leo <omar.deleo@gmail.com>
2019-04-11 21:52:29 -0400

    Create ThumbnailOverlay component

---
+++ frontend/components/ThumbnailOverlay.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function ThumbnailOverlay(props) {
+    return (
+        <div className="overlay">
+            <div className="overlay__data">
+                <div className="heart overlay-icon" />
+                {props.likes}{" "}
+            </div>
+            <div className="overlay__data">
+                <div className="comment overlay-icon" />
+                {props.comments}{" "}
+            </div>
+        </div>
+    );
+}
+
+export default ThumbnailOverlay;
