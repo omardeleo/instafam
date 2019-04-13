98c445c784c06bb8339d4461845689d55b7546ab
Omar De Leo <omar.deleo@gmail.com>
2019-04-12 20:20:24 -0400

    Update users reducer

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -1,15 +1,14 @@
 import merge from 'lodash/merge';
 
 import {
-    RECEIVE_CURRENT_USER,
     RECEIVE_USER
 } from '../actions/user_actions';
 
 const usersReducer = (state = {}, action) => {
     Object.freeze(state);
     switch(action.type) {
-        case RECEIVE_CURRENT_USER:
-            return merge({}, state, { [action.currentUser.id]: action.currentUser });
+        // case RECEIVE_CURRENT_USER:
+        //     return merge({}, state, { [action.currentUser.id]: action.currentUser });
         case RECEIVE_USER:
             return merge({}, state, { [action.user.id]: action.user });
         default:
