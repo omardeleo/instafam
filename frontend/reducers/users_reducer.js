dc921560d435607ef9fcea08cf81152e7ee2e699
Omar De Leo <omar.deleo@gmail.com>
2019-04-14 23:04:13 -0400

    Add RECEIVE_CURRENT_USER action

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -4,11 +4,15 @@ import {
     RECEIVE_USER
 } from '../actions/user_actions';
 
+import {
+    RECEIVE_CURRENT_USER
+} from '../actions/session_actions';
+
 const usersReducer = (state = {}, action) => {
     Object.freeze(state);
     switch(action.type) {
-        // case RECEIVE_CURRENT_USER:
-        //     return merge({}, state, { [action.currentUser.id]: action.currentUser });
+        case RECEIVE_CURRENT_USER:
+            return merge({}, state, { [action.currentUser.id]: action.currentUser });
         case RECEIVE_USER:
             return merge({}, state, { [action.user.id]: action.user });
         default:
