50f3547be570ca18a0605b42d37c9f5019a46a38
Omar De Leo <omar.deleo@gmail.com>
2019-04-15 14:27:34 -0400

    Update model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,11 @@ class User < ApplicationRecord
     
     attr_reader :password
 
-    validates :username, :password_digest, :session_token, presence: true
+    validates :username, :password_digest, :session_token, presence: true, length: { maximum: 30 }
     validates :username, uniqueness: true
     validates :password, length: { minimum: 6 }, allow_nil: true
+    validates :name, length: { maximum: 30 }
+    validates :bio, length: { maximum: 150 }
 
     after_initialize :ensure_session_token
 
