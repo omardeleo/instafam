e36e4264eca3aeefb4b9ef36b25aa73ba097c475
Omar De Leo <omar.deleo@gmail.com>
2019-04-14 23:05:45 -0400

    Update avatar settings

--- app/models/user.rb
+++ app/models/user.rb
@@ -44,7 +44,7 @@ class User < ApplicationRecord
     #     source: :followed
 
     def avatar 
-        return self.image.variant(combine_options: { resize: "x150", extent: "150x150", gravity: "center"})
+        return self.image.variant(combine_options: { resize: "x150^", extent: "150x150", gravity: "center"})
     end
 
     def self.find_by_credentials(username, password)
