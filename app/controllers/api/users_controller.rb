f24907f3926329117466d47ba822580ea57a2a52
Omar De Leo <omar.deleo@gmail.com>
2019-04-15 11:28:25 -0400

    Update UsersController

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -34,8 +34,7 @@ class Api::UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:username, :password, :name, :email, :image, :thumbnail)
-    # params.require(:user).permit(:username, :password, :image)
+    params.require(:user).permit(:username, :password, :name, :email, :image)
   end
 
 end
