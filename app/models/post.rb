39b4eececa36aa9a715884911b517e3d3cc13a2e
Omar De Leo <omar.deleo@gmail.com>
2019-04-15 13:43:09 -0400

    Validate post attachment presence

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,18 @@
 class Post < ApplicationRecord
   validates :author_id, presence: true
 
+  validate :ensure_image
+
   has_one_attached :image
 
   def thumbnail 
     return self.image.variant(combine_options: { resize: "x300^", extent: "300x300", gravity: "center"})
+  end
+
+  def ensure_image
+    unless self.image.attached?
+      errors[:image] << "Must be attached"
+    end
   end
 
   belongs_to :author,
