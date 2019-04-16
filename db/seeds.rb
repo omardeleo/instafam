54bb7b671f96f39535eb5cbe4545dc1a8ddc8795
Omar De Leo <omar.deleo@gmail.com>
2019-04-16 15:24:16 -0400

    Create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+
+seeds = [["demouser", "Demo User", "demo.jpg"],
+    ["pizza.24.7", "Pizza Lovers", "pizza.jpg"],
+    ["catsofinstafam", "Cats and meowr", "cat.jpg"],
+    ["mets_maniac", "Lifelong Mets fan #LGM", "mets.jpg"],
+    ["sciencefacts", "Science Facts", "science.jpg"],
+    ["classic.simpsons", "Classic Simpsons", "simpsons.jpg"],
+    ["retro_80s_90s", "Retro 80's and 90's", "retro.jpg"],
+    ["travel_nomad", "World Wanderer", "travel.jpg"],
+    ["artistic", "Art Page", "art.jpg"]]
+
+
+if (User.count == 0)
+    seeds.each_with_index do |seed, i|
+        user = User.create!(username: seed[0], password: "password", email: "example#{i}@example.com", name: seed[1])
+        file = open("https://s3.amazonaws.com/instafam-seeds/#{seed[2]}")
+        user.image.attach(io: file, filename: seed[2])
+    end
+
+    seeds.each do |seed|
+        user = User.where(username: seed[0]).first
+        file = open("https://s3.amazonaws.com/instafam-seeds/#{seed[2]}")
+        p = Post.new(author_id: user.id, caption: "First Instafam post! 😎🎉")
+        p.image.attach(io: file, filename: seed[2])
+        p.save!
+    end
+end
+
+Follow.destroy_all
+
+User.all.each do |user1|
+    id1 = user1.id
+    User.all.each do |user2|
+        id2 = user2.id
+        if (id1 != id2) 
+            Follow.create!(follower_id: id1, followee_id: id2)
+        end
+    end
+end
