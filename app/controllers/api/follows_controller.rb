0f525b825a44fd2eb0ae52b2f97bdf5fd7f6459f
Omar De Leo <omar.deleo@gmail.com>
2019-04-16 23:31:50 -0400

    Update follow HTTP response

--- app/controllers/api/follows_controller.rb
+++ app/controllers/api/follows_controller.rb
@@ -1,14 +1,31 @@
 class Api::FollowsController < ApplicationController
   def create
-    @follow = Follow.create(follow_params)
+    @follow = Follow.new
+    @follow.follower_id = params[:follower_id]
+    @follow.followee_id = params[:user_id]
+
     if @follow.save
-      render json: @follow
+      @users = User.where(:id => [params[:user_id], params[:follower_id]])
+      render 'api/users/index'
     else
       render json: @follow.errors.full_messages, status: 422
     end
   end
 
+  def destroy
+    @follow = Follow.where(follower_id: params[:follower_id], followee_id: params[:followee_id]).first
+    
+    if @follow
+      @follow.destroy!
+      @users = User.where(:id => [params[:followee_id], params[:follower_id]])
+      render 'api/users/index'
+    else
+      render json: ["invalid action"]
+    end
+  end
+
   def follow_params
-    params.require(:follow).permit(:follower_id, :followee_id)
+    params.require(:follow).permit(:follower_id, :followee_id, :user_id)
   end
 end
+
