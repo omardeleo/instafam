12f89caaeeecc84f5d11a4b60077156aa87edd46
Omar De Leo <omar.deleo@gmail.com>
2019-04-16 23:31:06 -0400

    Map follow actions to props

--- frontend/components/ProfileContainer.jsx
+++ frontend/components/ProfileContainer.jsx
@@ -2,10 +2,10 @@ import { connect } from 'react-redux';
 
 import { requestUserPosts, clearPosts } from '../actions/post_actions';
 import { updateUser, requestUser } from '../actions/user_actions';
+import { createFollow, deleteFollow } from '../actions/follow_actions';
 import Profile from './Profile';
 
 const mapStateToProps = (state, ownProps) => {
-    // debugger
     let userId = ownProps.match.params.userId ? 
         ownProps.match.params.userId : 
         state.session.id;
@@ -21,7 +21,9 @@ const mapDispatchToProps = (dispatch) => ({
     requestUserPosts: id => dispatch(requestUserPosts(id)),
     updateUser: (user, id) => dispatch(updateUser(user, id)),
     requestUser: id => dispatch(requestUser(id)),
-    clearPosts: () => dispatch(clearPosts())
+    clearPosts: () => dispatch(clearPosts()),
+    createFollow: (followerId, followeeId) => dispatch(createFollow(followerId, followeeId)),
+    deleteFollow: (followerId, followeeId) => dispatch(deleteFollow(followerId, followeeId))
 });
 
 export default connect(
