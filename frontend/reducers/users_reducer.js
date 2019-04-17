5fd2d92af98b1ec66caaef83842ba71732e96cab
Omar De Leo <omar.deleo@gmail.com>
2019-04-16 23:32:36 -0400

    Add RECEIVE_USERS action

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -1,7 +1,6 @@
-import merge from 'lodash/merge';
-
 import {
-    RECEIVE_USER
+    RECEIVE_USER,
+    RECEIVE_USERS
 } from '../actions/user_actions';
 
 import {
@@ -12,9 +11,11 @@ const usersReducer = (state = {}, action) => {
     Object.freeze(state);
     switch(action.type) {
         case RECEIVE_CURRENT_USER:
-            return merge({}, state, { [action.currentUser.id]: action.currentUser });
+            return Object.assign({}, state, { [action.currentUser.id]: action.currentUser });
+        case RECEIVE_USERS:
+            return Object.assign({}, state, action.users);
         case RECEIVE_USER:
-            return merge({}, state, { [action.user.id]: action.user });
+            return Object.assign({}, state, { [action.user.id]: action.user });
         default:
             return state;
     }
