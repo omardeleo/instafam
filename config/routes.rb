76fd2aab742d073a7839f8e0f76fb5e778208114
Omar De Leo <omar.deleo@gmail.com>
2019-04-16 23:30:18 -0400

    Add follow backend routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: {format: :json} do
-    resources :users, only: [:create, :show, :index, :update]
+    resources :users, only: [:create, :show, :index, :update] do 
+      resources :follows, only: [:create]
+    end
     resource :session, only: [:create, :destroy]
     resources :posts, only: [:create, :destroy, :show, :index, :update] do
       resources :likes, only: [:create, :destroy]
       resources :comments, only: [:create, :index]
     end
-    resources :follows, only: [:create]
     resources :comments, only: [:destroy]
+    resource :follows, only: [:destroy]
   end
   
   root 'static_pages#root'
